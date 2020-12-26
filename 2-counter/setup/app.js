392a2ef5fa9f14a627cb7a7f2182e336a550d18f
shedupte <shedupte@gmail.com>
2020-12-25 23:30:51 -0500

    Counter increase and decrease

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -3,6 +3,27 @@ const value= document.querySelector("#value");
 
 let count = 0;
 
-btn.addEventListener('click', function () {
+btns.forEach( function (btn) {
+btn.addEventListener('click', function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")){
+        count--;
+    } else if(styles.contains("increase")){
+        count ++;
+    } else{
+        count=0;
+    }
 
-})
+    if (count > 0){
+        value.style.color = "green";
+    }
+    if (count < 0){
+        value.style.color = "red";
+    }
+    if (count === 0) {
+        value.style.color = "#222";
+    }
+    value.textContent = count;
+    
+});
+});
