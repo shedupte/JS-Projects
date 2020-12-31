0fbe4a5a209b4475c5d9ca334f1767adff647c5c
shedupte <shedupte@gmail.com>
2020-12-30 21:42:51 -0500

    menu display function

--- 8-menu/setup/app.js
+++ 8-menu/setup/app.js
@@ -78,7 +78,11 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function () {
-  let displayMenu = menu.map(function(item){
+   displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
     //console.log(item);
 
     return `<article class="menu-item">
@@ -96,5 +100,4 @@ window.addEventListener('DOMContentLoaded', function () {
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-  console.log(displayMenu);
-});
+}
