ca9cd7cf943758181e91f7d87246f9f72730ad9f
shedupte <shedupte@gmail.com>
2020-12-30 19:58:23 -0500

    joining multiple menu items

--- 8-menu/setup/app.js
+++ 8-menu/setup/app.js
@@ -72,3 +72,29 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+//targetting section center to populate menu items
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function () {
+  let displayMenu = menu.map(function(item){
+    //console.log(item);
+
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.title}>
+    <div class="item-info">
+      <header>
+      <h4>${item.title}</h4>
+      <h4 class="price">${item.price}</h4>
+    </header>
+    <p class="item-text">
+      ${item.desc}
+    </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+  console.log(displayMenu);
+});
